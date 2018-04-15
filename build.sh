2ae5303caad91fc03006705929f03f116ef2bc06
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:18:22 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ sed -i '/"lib64"/s/64//' Modules/GNUInstallDirs.cmake && \
             --no-system-jsoncpp  \
             --no-system-librhash \
             --docdir=/share/doc/cmake-3.10.2 && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install
